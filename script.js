cd999b0339e22282d4b80663213793c4935aab1f
Sangee-R <109620687+Sangee-R@users.noreply.github.com>
2022-10-23 12:45:51 +0530

    Youtube clone

---
+++ script.js
@@ -0,0 +1,87 @@
+const videoCardContainer = document.querySelector(".video-container");
+
+/*let key = "AIzaSyAYq3VlsPOQUk90Y9lJKEhGTucJrcSpBx8";*/
+let key = "AIzaSyB4P8gLHMZMFCn9ayHZ-6jbrP09A2Phtl4";
+let http = "https://www.googleapis.com/youtube/v3/videos?";
+let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
+
+fetch(
+    http +
+    new URLSearchParams({
+        key: key,
+        part: "snippet",
+        chart: "mostPopular",
+        maxResults: 50,
+        regionCode: "US",
+    })
+)
+    .then((res) => res.json())
+    .then((data) => {
+        data.items.forEach((item) => {
+            getChannelIcon(item);
+        });
+    })
+    .catch((err) => console.log(err));
+
+const getChannelIcon = (video_data) => {
+    fetch(
+        channel_http +
+        new URLSearchParams({
+            key: key,
+            part: "snippet",
+            id: video_data.snippet.channelId,
+        })
+    )
+        .then((res) => res.json())
+        .then((data) => {
+            video_data.channelThumbnail =
+                data.items[0].snippet.thumbnails.default.url;
+            makeVideoCard(video_data);
+        });
+};
+
+const makeVideoCard = (data) => {
+    videoCardContainer.innerHTML += `
+    <div class="video" onclick="location.href = 'https://youtube.com/watch?v=${data.id}'">
+        <img src="${data.snippet.thumbnails.high.url}" class="thumbnail" alt="">
+        <div class="content">
+            <img src="${data.channelThumbnail}" class="channel-icon" alt="">
+            <div class="info">
+                <h4 class="title">${data.snippet.title}</h4>
+                <p class="channel-name">${data.snippet.channelTitle}</p>
+            </div>
+        </div>
+    </div>
+    `;
+};
+
+// search bar
+const searchInput = document.querySelector('#input');
+const searchBtn = document.querySelector('#basic-addon1');
+let searchLink = "https://www.youtube.com/results?search_query=";
+
+searchBtn.addEventListener('click', () => {
+    if (searchInput.value.length) {
+        location.href = searchLink + searchInput.value;
+    }
+})
+
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction1() {
+    var popup = document.getElementById("myPopup1");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction2() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
+
+  function myFunction3() {
+    var popup = document.getElementById("myPopup3");
+    popup.classList.toggle("show");
+  }
